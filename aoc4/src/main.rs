f47ff30557ed3e74586aa574ee8be2dae22a50bf
Garret Premo <garretpremo@gmail.com>
2023-01-23 16:36:08 -0500

    solution for 4.2

--- aoc4/src/main.rs
+++ aoc4/src/main.rs
@@ -6,8 +6,10 @@ fn main() {
     let assignment_pairs = parse_input_file("inputs/input4.txt");
 
     let enveloping_pairs = get_enveloping_pair_count(&assignment_pairs);
+    let overlapping_pairs = get_overlapping_pair_count(&assignment_pairs);
 
     println!("4.1 answer: {}", enveloping_pairs);
+    println!("4.2 answer: {}", overlapping_pairs);
 }
 
 #[test]
@@ -21,10 +23,29 @@ fn sample_part_1() {
     assert_eq!(enveloping_pairs, 2);
 }
 
+#[test]
+fn sample_part_2() {
+    let assignment_pairs = parse_input_file("../inputs/sample4.txt");
+
+    let overlapping_pairs = get_overlapping_pair_count(&assignment_pairs);
+
+    assert_eq!(overlapping_pairs, 4);
+}
+
 fn get_enveloping_pair_count(assignment_pairs: &Vec<AssignmentPair>) -> u32 {
     assignment_pairs.iter()
         .fold(0, |count, pair| {
             match pair.one_assignment_envelops_another() {
+                true => count + 1,
+                false => count
+            }
+        })
+}
+
+fn get_overlapping_pair_count(assignment_pairs: &Vec<AssignmentPair>) -> u32 {
+    assignment_pairs.iter()
+        .fold(0, |count, pair| {
+            match pair.has_overlapping_assignments() {
                 true => count + 1,
                 false => count
             }
--- aoc4/src/pair.rs
+++ aoc4/src/pair.rs
@@ -22,6 +22,12 @@ impl Assignment {
     fn envelops(&self, other: &Assignment) -> bool {
         self.start <= other.start && self.end >= other.end
     }
+
+    fn overlaps(&self, other: &Assignment) -> bool {
+        (self.start <= other.start && self.end >= other.start)
+            || (self.end >= other.end && self.start <= other.end)
+            || (self.start >= other.start && self.end <= other.end)
+    }
 }
 
 impl AssignmentPair {
@@ -39,6 +45,10 @@ impl AssignmentPair {
     pub fn one_assignment_envelops_another(&self) -> bool {
         self.a.envelops(&self.b) || self.b.envelops(&self.a)
     }
+
+    pub fn has_overlapping_assignments(&self) -> bool {
+        self.a.overlaps(&self.b)
+    }
 }
 
 #[test]
@@ -51,3 +61,19 @@ fn test_from() {
     assert_eq!(pair.b.end, 8);
 }
 
+#[test]
+fn test_overlaps() {
+    let non_overlapping_pair = AssignmentPair::from("2-4,6-8");
+    let overlapping_pair_1 = AssignmentPair::from("2-4,4-8");
+    let overlapping_pair_2 = AssignmentPair::from("2-4,1-2");
+    let overlapping_pair_3 = AssignmentPair::from("4-8,5-7");
+    let overlapping_pair_4 = AssignmentPair::from("2-3,1-4");
+    let overlapping_pair_5 = AssignmentPair::from("1-4,2-3");
+
+    assert_eq!(non_overlapping_pair.has_overlapping_assignments(), false);
+    assert_eq!(overlapping_pair_1.has_overlapping_assignments(), true);
+    assert_eq!(overlapping_pair_2.has_overlapping_assignments(), true);
+    assert_eq!(overlapping_pair_3.has_overlapping_assignments(), true);
+    assert_eq!(overlapping_pair_4.has_overlapping_assignments(), true);
+    assert_eq!(overlapping_pair_5.has_overlapping_assignments(), true);
+}
